622a6e1b4283342add1d092579448e674acf9093
vankulesh1 <113556145+vankulesh1@users.noreply.github.com>
2022-10-18 10:25:33 +0300

    Add files via upload

---
+++ Лаб 1-2/Лаб 1-2/Лаб 1-2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Лаб_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+
+            float I;
+            Console.WriteLine("Enter the value U: ");
+            float U = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value R: ");
+            float R = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value R: " + U);
+            Console.WriteLine("Enter the value U: " + R);
+
+            I = U / R;
+            Console.WriteLine("Value I = " + I);
+
+            
+        }
+    }
+}
