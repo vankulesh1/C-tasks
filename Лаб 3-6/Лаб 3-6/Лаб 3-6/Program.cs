80ae02262852792d64d931f2353b98379019727c
vankulesh1 <113556145+vankulesh1@users.noreply.github.com>
2022-10-18 10:25:00 +0300

    Add files via upload

---
+++ Лаб 3-6/Лаб 3-6/Лаб 3-6/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Лаб_3_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // лаб 3
+            //Console.WriteLine("Enter value x1: ");
+            //int x1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter value y1: ");
+            //int y1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter value x2: ");
+            //int x2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter value y2: ");
+            //int y2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (x1 == x2 || y1 == y2)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+            //лаб 6
+
+            // 1.
+            //Console.WriteLine("Write a number");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //while (x != 0)
+            //{
+            //    int a = x % 10;
+            //    x = x / 10;
+            //    Console.Write(a);
+            //}
+
+            // 2.
+
+
+
+        }
+    }
+}
